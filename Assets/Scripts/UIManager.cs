8e410ad10419f9f25b07747957717e54c03c549b
KimEoJin24 <kamejk24@gmail.com>
2023-09-08 22:18:17 +0900

    [ADD][DESIGN] PAUSE => Retry, Quit, Continue
    PAUSE 누르면 Retry, Quit, Continue 패널 나오게 구현

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public void ActivePauseButton()
+    {
+        Time.timeScale = 0;
+    }
     // Start is called before the first frame update
     void Start()
     {
