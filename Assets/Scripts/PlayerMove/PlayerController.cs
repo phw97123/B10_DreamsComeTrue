7ffbb63d8d87056b4256286450ab0aa860c8e4b1
KimEoJin24 <kamejk24@gmail.com>
2023-09-11 14:11:37 +0900

    [ADD] HUD, PlayerController

--- Assets/Scripts/PlayerMove/PlayerController.cs
+++ Assets/Scripts/PlayerMove/PlayerController.cs
@@ -6,9 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public event Action<Vector2> OnMoveEvent;
-    public int Hp = 5;
-    public int MaxHp = 5;
-    public int Score = 0;
+    public static int Score = 0;
     public void CallMoveEvent(Vector2 direction)
     {
         OnMoveEvent?.Invoke(direction);
@@ -18,10 +16,7 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.tag == "BadBug")
         {
-            if (Hp > 0)
-            {
-                Hp--;
-            }
+            
         }
         else if(other.gameObject.tag == "FixBug")
         {
@@ -29,10 +24,7 @@ public class PlayerController : MonoBehaviour
         }
         else if (other.gameObject.tag == "RecoveryItem")
         {
-            if (MaxHp > Hp)
-            {
-                Hp++;
-            }
+            
         }
         other.gameObject.SetActive(false);
     }
