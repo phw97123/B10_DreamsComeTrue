fa488a2f68078a369fb288d43dbd78a909414afd
phw97123 <phw97123@naver.com>
2023-09-11 16:15:23 +0900

    [Feat] 각 캐릭터 능력치 부여

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //캐릭터Animator
     public RuntimeAnimatorController[] characterAnimController;
     private GameObject player;
-    Animator playerAnimator;
+    private PlayerMoveMent playerMovement; 
+    private Animator playerAnimator;
 
     private void Awake()
     {
         player = GameObject.Find("Player");
+        playerMovement = player.GetComponent<PlayerMoveMent>(); 
 
+        //캐릭터 정보
         if (PlayerPrefs.HasKey("CharacterNumber"))
         {
             playerAnimator = player.GetComponent<Animator>();
             playerAnimator.runtimeAnimatorController = characterAnimController[PlayerPrefs.GetInt("CharacterNumber")];
+
+            //캐릭터 능력치 조정
+            switch(PlayerPrefs.GetInt("CharacterNumber"))
+            {
+                case 0:
+                    playerMovement.StunTime = 1.0f;
+                    playerMovement.Speed = 1;
+                    break; 
+                case 1:
+                    playerMovement.StunTime = 1.25f; 
+                    break;
+                case 2:
+                    playerMovement.StunTime = 0.75f;
+                    break;
+                case 3:
+                    playerMovement.Speed = 1.25f; 
+                    break;
+                default:
+                    Debug.Log("캐릭터가 없습니다."); 
+                    break; 
+            }
         }
     }
 
