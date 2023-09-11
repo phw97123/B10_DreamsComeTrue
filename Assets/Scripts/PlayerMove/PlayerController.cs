e413528a204dec2045542eee44bf1bfa809c56db
조범준 <141565207+KimMaYa1@users.noreply.github.com>
2023-09-11 20:08:06 +0900

    [Add] 스턴구현

--- Assets/Scripts/PlayerMove/PlayerController.cs
+++ Assets/Scripts/PlayerMove/PlayerController.cs
@@ -2,22 +2,40 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public event Action<Vector2> OnMoveEvent;
     public static int Score = 0;
     public static bool IsDead = false;
+    public float StunTime = 1;
+    private bool _isStun = false;
+    private float _time = 0;
     public void CallMoveEvent(Vector2 direction)
     {
         OnMoveEvent?.Invoke(direction);
+    }
+
+    void Update()
+    {
+        if (_isStun)
+        {
+            _time += Time.deltaTime;
+            if(_time > StunTime)
+            {
+                OutStun();
+                _time = 0;
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "BadBug")
         {
-            IsDead = true;
+            GetComponent<PlayerInput>().enabled = false;
+            _isStun = true;
         }
         else if(other.gameObject.tag == "FixBug")
         {
@@ -46,7 +64,14 @@ public class PlayerController : MonoBehaviour
         else if(other.gameObject.tag == "KillObject")
         {
             Debug.Log("KillObject");
+            IsDead = true;
         }
         other.gameObject.SetActive(false);
+    }
+
+    private void OutStun()
+    {
+        GetComponent<PlayerInput>().enabled = true;
+        _isStun = false;
     }
 }
