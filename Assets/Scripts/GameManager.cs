21ad1a528c73cb260ad38fde8ec25d715864df59
phw97123 <phw97123@naver.com>
2023-09-11 22:57:10 +0900

    [ADD] player 능력치 조정
    player의 능력치를 설정할 때 PlayerMovement에 있던 변수가 PlayerController로 이동하여 GameManager 변경

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,13 +7,15 @@ public class GameManager : MonoBehaviour
     //캐릭터Animator
     public RuntimeAnimatorController[] characterAnimController;
     private GameObject player;
-    //private PlayerMoveMent playerMovement; 
+    private PlayerMoveMent playerMovement;
+    private PlayerController playerController; 
     private Animator playerAnimator;
 
     private void Awake()
     {
         player = GameObject.Find("Player");
-        //playerMovement = player.GetComponent<PlayerMoveMent>(); 
+        playerMovement = player.GetComponent<PlayerMoveMent>();
+        playerController = player.GetComponent<PlayerController>(); 
 
         //캐릭터 정보
         if (PlayerPrefs.HasKey("CharacterNumber"))
@@ -23,25 +25,25 @@ public class GameManager : MonoBehaviour
             playerAnimator.runtimeAnimatorController = characterAnimController[PlayerPrefs.GetInt("CharacterNumber")];
 
             //캐릭터 능력치 조정
-            //switch(PlayerPrefs.GetInt("CharacterNumber"))
-            //{
-            //    case 0:
-            //        playerMovement.StunTime = 1.0f;
-            //        playerMovement.Speed = 1;
-            //        break; 
-            //    case 1:
-            //        playerMovement.StunTime = 1.25f; 
-            //        break;
-            //    case 2:
-            //        playerMovement.StunTime = 0.75f;
-            //        break;
-            //    case 3:
-            //        playerMovement.Speed = 1.25f; 
-            //        break;
-            //    default:
-            //        Debug.Log("캐릭터가 없습니다."); 
-            //        break; 
-            //}
+            switch (PlayerPrefs.GetInt("CharacterNumber"))
+            {
+                case 0:
+                    playerController.StunTime = 1.0f;
+                    playerMovement.Speed = 1;
+                    break;
+                case 1:
+                    playerController.StunTime = 1.25f;
+                    break;
+                case 2:
+                    playerController.StunTime = 0.75f;
+                    break;
+                case 3:
+                    playerMovement.Speed = 1.25f;
+                    break;
+                default:
+                    Debug.Log("캐릭터가 없습니다.");
+                    break;
+            }
         }
     }
 
