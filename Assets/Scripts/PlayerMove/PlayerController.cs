465c3b28d458d4feaebbf75c4c2cd3425dfd45fb
toadsam <toadsam@naver.com>
2023-09-12 09:16:14 +0900

    [ADD] 새로운 파일 받음 (-재훈)
    새로운 파일

--- Assets/Scripts/PlayerMove/PlayerController.cs
+++ Assets/Scripts/PlayerMove/PlayerController.cs
@@ -2,48 +2,95 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public event Action<Vector2> OnMoveEvent;
-    public int Hp = 5;
-    public int MaxHp = 5;
-    public int Score = 0;
+    public static int Score = 0;
+    public static bool IsDead = false;
+    public float StunTime = 1;
+    private bool _isStun = false;
+    private float _time = 0;
+
     public void CallMoveEvent(Vector2 direction)
     {
         OnMoveEvent?.Invoke(direction);
     }
 
+    void Update()
+    {
+        if (_isStun)
+        {
+            _time += Time.deltaTime;
+            if(_time > StunTime)
+            {
+                OutStun();
+                _time = 0;
+            }
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "BadBug")
+        switch (other.gameObject.tag)
         {
-            if (GameManager.instance.BugDieItem.activeSelf == true)//만약 활성화 되어있다면.
-            {
-                GameManager.instance.BugDieItemCount();  //얘네들도 함수로 만들기
-            }
-            if (Hp > 0)
-            {
-                
-                Hp--;
-            }
-        }
-        else if(other.gameObject.tag == "FixBug")
-        {
-            Score++;
-        }
-        else if (other.gameObject.tag == "RecoveryItem")
-        {
-            if (MaxHp > Hp)
-            {
-                Hp++;
-            }
-        }
-        else if (other.gameObject.tag == "Pesticides")
-        {
-            Debug.Log("나 닿았어");
-            GameManager.instance.BugDieItemOn();// 텍스트에서 점점 뺴는 것
+            case "BadBug":
+                GetComponent<PlayerInput>().enabled = false;
+                _isStun = true;
+                break;
+            case "FixBug":
+                Score++;
+                break;
+            case "Battery":
+                break;
+            case "ChatGpt":
+                break;
+            case "CPU":
+                break;
+            case "Insecticide":
+                break;
+            case "NullImage":
+                break;
+            case "KillObject":
+                IsDead = true;
+                break;
         }
         other.gameObject.SetActive(false);
     }
+
+    private void OutStun()
+    {
+        GetComponent<PlayerInput>().enabled = true;
+        _isStun = false;
+    }
+    //public void BugDieItemOn()//아이템 실행함수
+    //{
+    // PlayerController에서 BugDieItem가 활성화 되어있다면 마비 안하는걸로 조건문 걸기
+    //    if (BugDie == 16)
+    //    {
+    //        BugDieItemCountTxt.text = BugDie.ToString();
+    //        BugDieItem.SetActive(true);
+    //    }
+    //    else
+    //    {
+    //        BugDie = 16;
+    //        BugDieItemCountTxt.text = BugDie.ToString();
+    //        Debug.Log(BugDie + "임");
+    //    }
+
+
+    //}
+    //public void BugDieItemCount()
+    //{
+    //    BugDie -= 1;
+    //    BugDieItemCountTxt.text = BugDie.ToString();
+    //    Debug.Log(BugDie + "남음");
+    //    if (BugDie == 0)
+    //    {
+    //        Debug.Log("함수 실행이 끝났습니다");
+    //        BugDie = 16;
+    //        BugDieItem.SetActive(false);
+    //    }
+    //}
 }
